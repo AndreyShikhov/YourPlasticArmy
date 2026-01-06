bb2b85d5ae24d9a3e9994686555a44aa3eb38ccd
Andrey <Andrey.Shikhov94@yandex.ru>
2026-01-06 19:20:51 +0500

    add new army

--- YPA/lib/core/database/tables/seed/seed_objects/_types.dart
+++ YPA/lib/core/database/tables/seed/seed_objects/_types.dart
@@ -72,33 +72,191 @@ extension FactionCodeX on FactionCode {
 
 
 enum ArmyCode {
+  // Imperium
+  adeptaSororitas,
+  adeptusCustodes,
+  adeptusMechanicus,
+  adeptusTitanicus,
+  astraMilitarum,
+  greyKnights,
+  imperialAgents,
+  imperialKnights,
   spaceMarines,
+  // Chaos
+  chaosDaemons,
+  chaosKnights,
+  chaosSpaceMarines,
+  deathGuard,
+  emperorsChildren,
+  thousandSons,
+  worldEaters,
+  // Xenos
+  aeldari,
+  drukhari,
+  genestealerCults,
+  leaguesOfVotann,
+  necrons,
   orks,
+  tauEmpire,
+  tyranids,
+
 }
 extension ArmyCodeX on ArmyCode {
   String get code {
     switch (this) {
+     case ArmyCode.adeptaSororitas:
+        return 'adepta_sororitas';
+      case ArmyCode.adeptusCustodes:
+        return 'adeptus_custodes';
+      case ArmyCode.adeptusMechanicus:
+        return 'adeptus_mechanicus';
+      case ArmyCode.adeptusTitanicus:
+        return 'adeptus_titanicus';
+      case ArmyCode.astraMilitarum:
+        return 'astra_militarum';
+      case ArmyCode.greyKnights:
+        return 'grey_knights';
+      case ArmyCode.imperialAgents:
+        return 'imperial_agents';
+      case ArmyCode.imperialKnights:
+        return 'imperial_knights';
       case ArmyCode.spaceMarines:
         return 'space_marines';
+      case ArmyCode.chaosDaemons:
+        return 'chaos_daemons';
+      case ArmyCode.chaosKnights:
+        return 'chaos_knights';
+      case ArmyCode.chaosSpaceMarines:
+        return 'chaos_space_marines';
+      case ArmyCode.deathGuard:
+        return 'death_guard';
+      case ArmyCode.emperorsChildren:
+        return 'emperors_children';
+      case ArmyCode.thousandSons:
+        return 'thousand_sons';
+      case ArmyCode.worldEaters:
+        return 'world_eaters';
+      case ArmyCode.aeldari:
+        return 'aeldari';
+      case ArmyCode.drukhari:
+        return 'drukhari';
+      case ArmyCode.genestealerCults:
+        return 'genestealer_cults';
+      case ArmyCode.leaguesOfVotann:
+        return 'leagues_of_votann';
+      case ArmyCode.necrons:
+        return 'necrons';
       case ArmyCode.orks:
         return 'orks';
+      case ArmyCode.tauEmpire:
+        return 'tau_empire';
+      case ArmyCode.tyranids:
+        return 'tyranids';
     }
   }
 
   String get title {
     switch (this) {
+      case ArmyCode.adeptaSororitas:
+        return 'Adepta Sororitas';
+      case ArmyCode.adeptusCustodes:
+        return 'Adeptus Custodes';
+      case ArmyCode.adeptusMechanicus:
+        return 'Adeptus Mechanicus';
+      case ArmyCode.adeptusTitanicus:
+        return 'Adeptus Titanicus';
+      case ArmyCode.astraMilitarum:
+        return 'Astra Militarum';
+      case ArmyCode.greyKnights:
+        return 'Grey Knights';
+      case ArmyCode.imperialAgents:
+        return 'Imperial Agents';
+      case ArmyCode.imperialKnights:
+        return 'Imperial Knights';
       case ArmyCode.spaceMarines:
         return 'Space Marines';
+      case ArmyCode.chaosDaemons:
+        return 'Chaos Daemons';
+      case ArmyCode.chaosKnights:
+        return 'Chaos Knights';
+      case ArmyCode.chaosSpaceMarines:
+        return 'Chaos Space Marines';
+      case ArmyCode.deathGuard:
+        return 'Death Guard';
+      case ArmyCode.emperorsChildren:
+        return 'Emperors Children';
+      case ArmyCode.thousandSons:
+        return 'Thousand Sons';
+      case ArmyCode.worldEaters:
+        return 'World Eaters';
+      case ArmyCode.aeldari:
+        return 'Aeldari';
+      case ArmyCode.drukhari:
+        return 'Drukhari';
+      case ArmyCode.genestealerCults:
+        return 'Genestealer Cults';
+      case ArmyCode.leaguesOfVotann:
+        return 'Leagues of Votann';
+      case ArmyCode.necrons:
+        return 'Necrons';
       case ArmyCode.orks:
         return 'Orks';
+      case ArmyCode.tauEmpire:
+        return 'Tau Empire';
+      case ArmyCode.tyranids:
+        return 'Tyranids';
     }
   }
 
   FactionCode get faction {
     switch (this) {
+     case ArmyCode.adeptaSororitas:
+       return FactionCode.imperium;
+      case ArmyCode.adeptusCustodes:
+        return FactionCode.imperium;
+      case ArmyCode.adeptusMechanicus:
+        return FactionCode.imperium;
+      case ArmyCode.adeptusTitanicus:
+        return FactionCode.imperium;
+      case ArmyCode.astraMilitarum:
+        return FactionCode.imperium;
+      case ArmyCode.greyKnights:
+        return FactionCode.imperium;
+      case ArmyCode.imperialAgents:
+        return FactionCode.imperium;
+      case ArmyCode.imperialKnights:
+        return FactionCode.imperium;
       case ArmyCode.spaceMarines:
         return FactionCode.imperium;
+      case ArmyCode.chaosDaemons:
+        return FactionCode.chaos;
+      case ArmyCode.chaosKnights:
+        return FactionCode.chaos;
+      case ArmyCode.chaosSpaceMarines:
+        return FactionCode.chaos;
+      case ArmyCode.deathGuard:
+        return FactionCode.chaos;
+      case ArmyCode.emperorsChildren:
+        return FactionCode.chaos;
+      case ArmyCode.thousandSons:
+        return FactionCode.chaos;
+      case ArmyCode.worldEaters:
+        return FactionCode.chaos;
+      case ArmyCode.aeldari:
+        return FactionCode.xenos;
+      case ArmyCode.drukhari:
+        return FactionCode.xenos;
+      case ArmyCode.genestealerCults:
+        return FactionCode.xenos;
+      case ArmyCode.leaguesOfVotann:
+        return FactionCode.xenos;
+      case ArmyCode.necrons:
+        return FactionCode.xenos;
       case ArmyCode.orks:
+        return FactionCode.xenos;
+      case ArmyCode.tauEmpire:
+        return FactionCode.xenos;
+      case ArmyCode.tyranids:
         return FactionCode.xenos;
     }
   }
--- YPA/lib/core/database/tables/seed/seed_objects/armies/army.dart
+++ YPA/lib/core/database/tables/seed/seed_objects/armies/army.dart
@@ -5,13 +5,115 @@ import '../_types.dart';
 List<ArmySeed> getAllArmies() {
   return
   [
+    
+    ArmySeed(
+        armyCode: ArmyCode.adeptaSororitas, 
+        armyName: ArmyCodeX(ArmyCode.adeptaSororitas).title, 
+        factionCode: FactionCode.imperium
+    ),
+    ArmySeed(
+        armyCode: ArmyCode.adeptusCustodes,
+        armyName: ArmyCodeX(ArmyCode.adeptusCustodes).title,
+        factionCode: FactionCode.imperium
+    ),
+    ArmySeed(
+        armyCode: ArmyCode.adeptusMechanicus,
+        armyName: ArmyCodeX(ArmyCode.adeptusMechanicus).title,
+        factionCode: FactionCode.imperium
+    ),
+    ArmySeed(
+        armyCode: ArmyCode.astraMilitarum,
+        armyName: ArmyCodeX(ArmyCode.astraMilitarum).title,
+        factionCode: FactionCode.imperium
+    ),
+    ArmySeed(
+        armyCode: ArmyCode.greyKnights,
+        armyName: ArmyCodeX(ArmyCode.greyKnights).title,
+        factionCode: FactionCode.imperium
+    ),
+    ArmySeed(
+        armyCode: ArmyCode.imperialKnights,
+        armyName: ArmyCodeX(ArmyCode.imperialKnights).title,
+        factionCode: FactionCode.imperium
+    ),
     ArmySeed(
         armyCode: ArmyCode.spaceMarines,
         armyName: ArmyCodeX(ArmyCode.spaceMarines).title,
-        factionCode: FactionCode.imperium),
+        factionCode: FactionCode.imperium
+    ),
+    ArmySeed(
+        armyCode: ArmyCode.chaosDaemons,
+        armyName: ArmyCodeX(ArmyCode.chaosDaemons).title,
+        factionCode: FactionCode.chaos
+    ),
+    ArmySeed(
+        armyCode: ArmyCode.chaosKnights,
+        armyName: ArmyCodeX(ArmyCode.chaosKnights).title,
+        factionCode: FactionCode.chaos
+    ),
+    ArmySeed(
+        armyCode: ArmyCode.chaosSpaceMarines,
+        armyName: ArmyCodeX(ArmyCode.chaosSpaceMarines).title,
+        factionCode: FactionCode.chaos
+    ),
+    ArmySeed(
+        armyCode: ArmyCode.deathGuard,
+        armyName: ArmyCodeX(ArmyCode.deathGuard).title,
+        factionCode: FactionCode.chaos
+    ),
+    ArmySeed(
+        armyCode: ArmyCode.emperorsChildren,
+        armyName: ArmyCodeX(ArmyCode.emperorsChildren).title,
+        factionCode: FactionCode.chaos
+    ),
+    ArmySeed(
+        armyCode: ArmyCode.thousandSons,
+        armyName: ArmyCodeX(ArmyCode.thousandSons).title,
+        factionCode: FactionCode.chaos
+    ),
+    ArmySeed(
+        armyCode: ArmyCode.worldEaters,
+        armyName: ArmyCodeX(ArmyCode.worldEaters).title,
+        factionCode: FactionCode.chaos
+    ),
+    ArmySeed(
+        armyCode: ArmyCode.aeldari,
+        armyName: ArmyCodeX(ArmyCode.aeldari).title,
+        factionCode: FactionCode.xenos
+    ),
+    ArmySeed(
+        armyCode: ArmyCode.drukhari,
+        armyName: ArmyCodeX(ArmyCode.drukhari).title,
+        factionCode: FactionCode.xenos
+    ),
+    ArmySeed(
+        armyCode: ArmyCode.genestealerCults,
+        armyName: ArmyCodeX(ArmyCode.genestealerCults).title,
+        factionCode: FactionCode.xenos
+    ),
+    ArmySeed(
+        armyCode: ArmyCode.leaguesOfVotann,
+        armyName: ArmyCodeX(ArmyCode.leaguesOfVotann).title,
+        factionCode: FactionCode.xenos
+    ),
+    ArmySeed(
+        armyCode: ArmyCode.necrons,
+        armyName: ArmyCodeX(ArmyCode.necrons).title,
+        factionCode: FactionCode.xenos
+    ),
     ArmySeed(
         armyCode: ArmyCode.orks,
         armyName: ArmyCodeX(ArmyCode.orks).title,
-        factionCode: FactionCode.xenos),
+        factionCode: FactionCode.xenos
+    ),
+    ArmySeed(
+        armyCode: ArmyCode.tauEmpire,
+        armyName: ArmyCodeX(ArmyCode.tauEmpire).title,
+        factionCode: FactionCode.xenos
+    ), ArmySeed(
+      armyCode: ArmyCode.tyranids,
+      armyName: ArmyCodeX(ArmyCode.tyranids).title,
+      factionCode: FactionCode.xenos
+  ),
   ];
 }
