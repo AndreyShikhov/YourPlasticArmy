fd1eae8eeca3d4122f684d8b5e5d0d8a814e3d82
Vinni1337 <sharov.mac@yandex.ru>
2026-01-22 13:16:24 +0500

    Детачи маров
    Добавил все детачи маров

--- YPA/lib/core/database/tables/seed/seed_objects/detachments/detachments.dart
+++ YPA/lib/core/database/tables/seed/seed_objects/detachments/detachments.dart
@@ -143,6 +143,207 @@ List<DetachmentSeed> detachmentsBase() {
       ruleFull: 'Rage Cursed Onslaught',
     ),
 
+    //=============Dark angels============
+    DetachmentSeed(
+        code: 'unforgiven_task_force',
+        armyCode: ArmyCode.spaceMarines,
+        name: 'Unforgiven Task Force',
+        description: 'Unforgiven Task Force',
+        ruleShort: 'Unforgiven Task Force',
+        ruleName: 'Unforgiven Task Force',
+        ruleFull: 'Unforgiven Task Force'
+    ),
+
+    DetachmentSeed(
+        code: 'inner_circle_task_force',
+        armyCode: ArmyCode.spaceMarines,
+        name: 'Inner Circle Task Force',
+        description: 'Inner Circle Task Force',
+        ruleShort: 'Inner Circle Task Force',
+        ruleName: 'Inner Circle Task Force',
+        ruleFull: 'Inner Circle Task Force'
+    ),
+    DetachmentSeed(
+        code: 'company_of_hunters',
+        armyCode: ArmyCode.spaceMarines,
+        name: 'Company of Hunters',
+        description: 'Company of Hunters',
+        ruleShort: 'Company of Hunters',
+        ruleName: 'Company of Hunters',
+        ruleFull: 'Company of Hunters'
+    ),
+    DetachmentSeed(
+        code: 'wrath_of_the_rock',
+        armyCode: ArmyCode.spaceMarines,
+        name: 'Wrath of the Rock',
+        description: 'Wrath of the Rock',
+        ruleShort: 'Wrath of the Rock',
+        ruleName: 'Wrath of the Rock',
+        ruleFull: 'Wrath of the Rock'
+    ),
+    DetachmentSeed(
+        code: 'lions_blade_task_force',
+        armyCode: ArmyCode.spaceMarines,
+        name: 'Lion’s Blade Task Force',
+        description: 'Lion’s Blade Task Force',
+        ruleShort: 'Lion’s Blade Task Force',
+        ruleName: 'Lion’s Blade Task Force',
+        ruleFull: 'Lion’s Blade Task Force'
+    ),
+    //=============Black templars============
+    DetachmentSeed(
+        code: 'companions_of_vehemence',
+        armyCode: ArmyCode.spaceMarines,
+        name: 'Companions of Vehemence',
+        description: 'Companions of Vehemence',
+        ruleShort: 'Companions of Vehemence',
+        ruleName: 'Companions of Vehemence',
+        ruleFull: 'Companions of Vehemence'
+    ),
+    DetachmentSeed(
+        code: 'vindication_task_force',
+        armyCode: ArmyCode.spaceMarines,
+        name: 'Vindication Task Force',
+        description: 'Vindication Task Force',
+        ruleShort: 'Vindication Task Force',
+        ruleName: 'Vindication Task Force',
+        ruleFull: 'Vindication Task Force'
+    ),
+    DetachmentSeed(
+        code: 'godhammer_assault_force',
+        armyCode: ArmyCode.spaceMarines,
+        name: 'Godhammer Assault Force',
+        description: 'Godhammer Assault Force',
+        ruleShort: 'Godhammer Assault Force',
+        ruleName: 'Godhammer Assault Force',
+        ruleFull: 'Godhammer Assault Force'
+    ),
+    DetachmentSeed(
+        code: 'wrathful_procession',
+        armyCode: ArmyCode.spaceMarines,
+        name: 'Wrathful Procession',
+        description: 'Wrathful Procession',
+        ruleShort: 'Wrathful Procession',
+        ruleName: 'Wrathful Procession',
+        ruleFull: 'Wrathful Procession'
+    ),
+    //=============Space wolfs============
+    DetachmentSeed(
+        code: 'saga_of_the_hunter',
+        armyCode: ArmyCode.spaceMarines,
+        name: 'Saga of the Hunter',
+        description: 'Saga of the Hunter',
+        ruleShort: 'Saga of the Hunter',
+        ruleName: 'Saga of the Hunter',
+        ruleFull: 'Saga of the Hunter'
+    ),
+    DetachmentSeed(
+        code: 'saga_of_the_bold',
+        armyCode: ArmyCode.spaceMarines,
+        name: 'Saga of the Bold',
+        description: 'Saga of the Bold',
+        ruleShort: 'Saga of the Bold',
+        ruleName: 'Saga of the Bold',
+        ruleFull: 'Saga of the Bold'
+    ),
+    DetachmentSeed(
+        code: 'saga_of_the_beastslayer',
+        armyCode: ArmyCode.spaceMarines,
+        name: 'Saga of the Beastslayer',
+        description: 'Saga of the Beastslayer',
+        ruleShort: 'Saga of the Beastslayer',
+        ruleName: 'Saga of the Beastslayer',
+        ruleFull: 'Saga of the Beastslayer'
+    ),
+    DetachmentSeed(
+        code: 'champions_of_fenris',
+        armyCode: ArmyCode.spaceMarines,
+        name: 'Champions of Fenris',
+        description: 'Champions of Fenris',
+        ruleShort: 'Champions of Fenris',
+        ruleName: 'Champions of Fenris',
+        ruleFull: 'Champions of Fenris'
+    ),
+    DetachmentSeed(
+        code: 'saga_of_the_great_wolf',
+        armyCode: ArmyCode.spaceMarines,
+        name: 'Saga of the Great Wolf',
+        description: 'Saga of the Great Wolf',
+        ruleShort: 'Saga of the Great Wolf',
+        ruleName: 'Saga of the Great Wolf',
+        ruleFull: 'Saga of the Great Wolf'
+    ),
+    //=============Deathwatch============
+    DetachmentSeed(
+        code: 'black_spear_task_force',
+        armyCode: ArmyCode.spaceMarines,
+        name: 'Black Spear Task Force',
+        description: 'Black Spear Task Force',
+        ruleShort: 'Black Spear Task Force',
+        ruleName: 'Black Spear Task Force',
+        ruleFull: 'Black Spear Task Force'
+    ),
+    //=============Imperial fists============
+    DetachmentSeed(
+        code: 'emperors_shield',
+        armyCode: ArmyCode.spaceMarines,
+        name: 'Emperor’s Shield',
+        description: 'Emperor’s Shield',
+        ruleShort: 'Emperor’s Shield',
+        ruleName: 'Emperor’s Shield',
+        ruleFull: 'Emperor’s Shield'
+    ),
+    //=============Iron Hands===========
+    DetachmentSeed(
+        code: 'hammer_of_avernii',
+        armyCode: ArmyCode.spaceMarines,
+        name: 'Hammer of Avernii',
+        description: 'Hammer of Avernii',
+        ruleShort: 'Hammer of Avernii',
+        ruleName: 'Hammer of Avernii',
+        ruleFull: 'Hammer of Avernii'
+    ),
+    //=============Raven guard===========
+    DetachmentSeed(
+        code: 'shadowmark_talon',
+        armyCode: ArmyCode.spaceMarines,
+        name: 'Shadowmark Talon',
+        description: 'Shadowmark Talon',
+        ruleShort: 'Shadowmark Talon',
+        ruleName: 'Shadowmark Talon',
+        ruleFull: 'Shadowmark Talon'
+    ),
+    //=============Salamanders===========
+    DetachmentSeed(
+        code: 'forgefatherss_seekers',
+        armyCode: ArmyCode.spaceMarines,
+        name: 'Forgefather’s Seekers',
+        description: 'Forgefather’s Seekers',
+        ruleShort: 'Forgefather’s Seekers',
+        ruleName: 'Forgefather’s Seekers',
+        ruleFull: 'Forgefather’s Seekers'
+    ),
+    //=============Ultramarines===========
+    DetachmentSeed(
+        code: 'blade_of_ultramar',
+        armyCode: ArmyCode.spaceMarines,
+        name: 'Blade of Ultramar',
+        description: 'Blade of Ultramar',
+        ruleShort: 'Blade of Ultramar',
+        ruleName: 'Blade of Ultramar',
+        ruleFull: 'Blade of Ultramar'
+    ),
+    //=============White scars===========
+    DetachmentSeed(
+        code: 'spearpoint_task_force',
+        armyCode: ArmyCode.spaceMarines,
+        name: 'Spearpoint Task Force',
+        description: 'Spearpoint Task Force',
+        ruleShort: 'Spearpoint Task Force',
+        ruleName: 'Spearpoint Task Force',
+        ruleFull: 'Spearpoint Task Force'
+    ),
+
 
   ];
 }
